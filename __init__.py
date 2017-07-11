0be162df232d5580787775d04597e73f83b984a1
Aaron Olkin <aaronolkin@gmail.com>
2017-07-11 00:22:41 -0400

    Added get_int

--- __init__.py
+++ __init__.py
@@ -10,21 +10,24 @@ class _Config:
     def _ready(self):
         self.__models = import_module("config.models")
         
-    def __getattr__(self,key):
+    def __getattr__(self, key):
         return self.__models.Config.get(key,nocache=not self._use_cache)
     __getitem__ = __getattr__
 
-    def __setattr__(self,key,value):
+    def __setattr__(self, key, value):
         if key.startswith("_"):
             return object.__setattr__(self, key, value)
         return self.__models.Config.set(key,value)
     __setitem__ = __setattr__
 
-    def get(self,key,fallback=None):
+    def get(self, key, fallback=None):
         return self.__models.Config.get(key, fallback,
                                         nocache=not self._use_cache)
 
-    def get_float(self,key,fallback=None):
+    def get_int(self, key, fallback=None):
+        return int(self.get_float(key, fallback))
+    
+    def get_float(self, key, fallback=None):
         try:
             return float(self.get(key,fallback))
         except ValueError as err:
