3fa5f577a1f12002c35a7e70f0df1318154eba5f
Aaron Olkin <aaronolkin@gmail.com>
2024-06-18 23:11:23 -0400

    Don't fault if the 'key' parameter is missing from an autocomplete request

--- views.py
+++ views.py
@@ -4,6 +4,8 @@ from .models import Autocomplete
 from django.http import JsonResponse
 
 def autocomplete_json(request):
+    if not "key" in request.GET:
+        return JsonResponse([], safe=False)
     qs = Autocomplete.objects.filter(
         key=request.GET["key"],
         value__icontains=request.GET.get("q", ""))
